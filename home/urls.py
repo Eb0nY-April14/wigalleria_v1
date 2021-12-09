22d8e63fd7b829dac3e9c5b873456095f38eee2e
Eb0nY-April14 <laratemi14@gmail.com>
2021-12-09 04:26:39 +0000

    Add home page content

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+# The index used here is same as store used
+# in the Django tutorial on YouTube
 urlpatterns = [
     path('', views.index, name='home')
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. The index.html/index view are 
+# same as store.html/store view used in the Django 
+# tutorial on YouTube
 def index(request):
     """ A view to return the index page """
 
