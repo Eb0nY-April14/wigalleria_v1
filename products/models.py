4008feac2e7e6f9d8e255fc344d0666dc9509ae3
Eb0nY-April14 <laratemi14@gmail.com>
2021-12-27 03:13:43 +0000

    Create models & fixtures

--- products/models.py
+++ products/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
@@ -25,4 +23,6 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     likes = models.ManyToManyField(User, related_name='product_like', blank=True)
-    
+
+    def __str__(self):
+        return self.name
