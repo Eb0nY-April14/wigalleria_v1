70b4ac6f8613cfc1fe3663c96990ba551a0c1200
Eb0nY-April14 <laratemi14@gmail.com>
2022-01-04 02:10:16 +0000

    Add the add to cart functionality

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here. The index.html/index view are 
-# same as store.html/store view used in the Django 
+# Create your views here. The index.html/index view are
+# same as store.html/store view used in the Django
 # tutorial on YouTube
 def index(request):
     """ A view to return the index page """
