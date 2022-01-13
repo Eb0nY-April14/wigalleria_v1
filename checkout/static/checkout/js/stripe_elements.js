3bfeb583f58b25c55fdfe6657a89b386c945a3c3
Eb0nY-April14 <laratemi14@gmail.com>
2022-01-13 18:09:28 +0000

    Complete confirmation emails

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -117,7 +117,7 @@ form.addEventListener('submit', function(ev) {
         }
     });
     }).fail(function () {
-        // just reload the page, the error will be in django messages
+        // just reload the page, the error message will be in django messages
     location.reload();
     })    
 });
