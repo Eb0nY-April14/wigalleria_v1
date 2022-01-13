eac3f22e80ca99afe7997322727dace8760b500a
Eb0nY-April14 <laratemi14@gmail.com>
2022-01-13 19:03:57 +0000

    Start Product Admin - Product form

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
-from .models import Comment
 from django import forms
+from .models import Comment, Product, Category
 
 
 class CommentForm(forms.ModelForm):
@@ -11,3 +11,21 @@ class CommentForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    # This function below will make us see the friendly names  
+    # in the form instead of the category ID or the name field.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'        
