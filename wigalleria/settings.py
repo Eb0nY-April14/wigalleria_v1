747f1ada4c009c3ac54514fd106d11473381d625
Eb0nY-April14 <laratemi14@gmail.com>
2022-01-14 22:06:23 +0000

    Remove secret key and set debug

--- wigalleria/settings.py
+++ wigalleria/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'pfb6r1bkbv_b48#r%r(eu^%k!es_jn1wbvr2w-p7srw%ly-va)')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 # Here, debug will be set to true only if there's a variable called
 # 'development' in the environment.
