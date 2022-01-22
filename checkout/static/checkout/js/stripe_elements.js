8ff6a54eb13c822473d7054ee1803934e939cd6c
Eb0nY-April14 <laratemi14@gmail.com>
2022-01-22 23:17:48 +0000

    Remove javascript type validation error

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -27,7 +27,6 @@ var style = {
 };
 
 var card = elements.create('card', {style: style});
-// var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
