8fb6e3d2cab09f668a4825d9e341116a18ebd19d
Eb0nY-April14 <laratemi14@gmail.com>
2022-11-26 18:44:42 +0000

    add build file & update settings & env files

--- wigalleria/settings.py
+++ wigalleria/settings.py
@@ -35,6 +35,11 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cyot14-wigalleria.herokuapp.com', 'localhost']
 
+ # Add Render.com URL to allowed hosts
+ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
